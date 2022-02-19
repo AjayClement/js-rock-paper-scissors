64603fefff779f76e181e6e92fd97d9d9a588260
Ajay Clement Selvaraj <clementajay@gmail.com>
2022-02-19 15:07:38 +0530

    add index.htm and script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+
+
+game();
+
+function playerPlay(){
+    const playerWeapon = prompt("Enter your weapon of choice",'');
+    console.log(`player's choice ${playerWeapon}`);
+    return playerWeapon;
+}
+
+function computerPlay(){
+    const weapons=["rock","paper","scissors"];
+    const max= weapons.length+1;
+    const randomNum = Math.floor(Math.random()*max);
+    console.log(`computer's choice ${weapons[randomNum]}`);
+    return weapons[randomNum]; 
+}
+
+
+function game(){
+  for(let i=0; i<=0;i++){
+
+    let playerSelection = playerPlay().toLowerCase();
+    let computerSelection = computerPlay().toLowerCase();
+
+    if(playerSelection===computerSelection){
+             console.log("It's a draw")
+    }else{
+            switch(playerSelection){
+                case "rock":
+                   if(computerSelection==='paper'){
+                    console.log(`You Lose!, ${computerSelection} beats ${playerSelection}`);
+                   }else{
+                    console.log(`You Win!, ${playerSelection} beats ${computerSelection}`);
+                   }
+                   break;
+                case "paper":
+                    if(computerSelection==='scissors'){
+                     console.log(`You Lose!, ${computerSelection} beats ${playerSelection}`);
+                    }else{
+                     console.log(`You Win!, ${playerSelection} beats ${computerSelection}`);
+                    }
+                    break;
+                case "scissors":
+                    if(computerSelection==='rock'){
+                     console.log(`You Lose!, ${computerSelection} beats ${playerSelection}`);
+                    }else{
+                     console.log(`You Win!, ${playerSelection} beats ${computerSelection}`);
+                    }
+                    break;
+                 default:
+                     console.log("Please choose a valid weapon");
+            }
+        }
+    }
+}
+
